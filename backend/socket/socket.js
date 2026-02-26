72506ae76262a0e9d92fe1c9377ef03e293d08fd
Furqan Ahmad <133624627+furqanahmadganaie@users.noreply.github.com>
2026-02-26 17:28:15 +0530

    sockets updated

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-// import { apiurl } from "../../frontend/src/constents/constents.js";
+
 
 const app = express();
 
