ae7a618a284262a54715e02a1b7fdec68411e261
Furqan Ahmad <133624627+furqanahmadganaie@users.noreply.github.com>
2026-02-26 17:29:22 +0530

    utils updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,17 +5,10 @@ const generateToken = (userId,res) =>{
   const token= jwt.sign({userId}, process.env.JWT_SECRET,{
     expiresIn:'15d',
   });
-//set an cookie
-  // res.cookie('jwt',token,{
-  //   maxAge:15 * 24 * 60 * 60 *1000,
-  //   httpOnly:true,                  //prevent from xss attacks  crosss site scripting attacks 
-  //   sameSite:"strict" ,               //prevents csrf attacks 
-  //   secure: process.env.NODE_ENV !=="development"
-  // });
-  // console.log('Generated Token:', token);
+
 
   return token;
    
 };
-  // coll this function in controllers
+  
 export default generateToken
